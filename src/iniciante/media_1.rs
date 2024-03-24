ab410cea7f89b8714ad4c424410dcdcc195ffc3a
kevenwallace <kevenwallace_@hotmail.com>
2024-03-24 18:51:26 -0300

    media1

---
+++ src/iniciante/media_1.rs
@@ -0,0 +1,21 @@
+use std::io;
+
+pub fn media1(){
+    //Exercicio 1005
+    let mut number_a:String = String::new();
+    let mut number_b:String = String::new();
+
+    io::stdin()
+        .read_line(&mut number_a)
+        .expect("error");
+
+    io::stdin()
+        .read_line(&mut number_b)
+        .expect("error");
+
+    let float_a:f32 = if let Ok(number) = number_a.trim().parse() {number} else {println!("error"); return;};
+    let float_b:f32 = if let Ok(number) = number_b.trim().parse() {number} else {println!("error"); return;};
+
+    let result:f32 = ((float_a * 3.5) + (float_b * 7.5) ) / 11f32;
+    println!("MEDIA = {:.5}", result)
+}
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ mod iniciante {
     pub mod area_do_circulo;
     pub mod soma_simples;
     pub mod produto_simples;
+    pub mod media_1;
 }
 
 #[allow(dead_code)]
@@ -12,12 +13,14 @@ use iniciante::{
     hello_world::funcao,
     area_do_circulo::area_do_circulo,
     soma_simples::soma_simples,
-    produto_simples::produto_simples
+    produto_simples::produto_simples,
+    media_1::media1
 };
 
 fn main() {
     // funcao();
     // extremamente_basico();
     // area_do_circulo();
-    produto_simples();
+    // produto_simples();
+    media1()
 }
