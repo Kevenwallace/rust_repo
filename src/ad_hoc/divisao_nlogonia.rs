8a2507a91613a70a6242534d6d3f30afa44f52d4
kevenwallace <kevenwallace_@hotmail.com>
2024-03-26 00:24:00 -0300

    resolvido

--- src/ad_hoc/divisao_nlogonia.rs
+++ src/ad_hoc/divisao_nlogonia.rs
@@ -3,15 +3,20 @@ use std::io;
 pub fn divisao_nlogonia() {
     //Exercicio 1091
     //https://judge.beecrowd.com/pt/problems/view/1091
+    let mut resultdo: Vec<&str> = Vec::new();
+
+    loop {
     let mut kuantity:String = String::new();
     io::stdin().read_line(&mut kuantity).expect("error");
     let kuantity_int:i16 = kuantity.trim().parse().expect("error parse");
-    
+    if kuantity_int == 0{break;}
     let mut value_cordenade:String = String::new();
     io::stdin().read_line(&mut value_cordenade).expect("error");
     
     let corden_split:Vec<&str> = value_cordenade.split_whitespace().collect();
-    println!("{:?}", corden_split);
+    let mut corden_split_int:Vec<i16> = Vec::with_capacity(2);
+    for l in corden_split {corden_split_int.push(l.trim().parse().expect("error parse"))}
+
 
     let mut vetor:Vec<Vec<i16>> = Vec::new();
     for _ in 0..kuantity_int{
@@ -26,7 +31,15 @@ pub fn divisao_nlogonia() {
         }
         vetor.push(int_splited);
     }
+    for k in &vetor{
+        resultdo.push(if k[0] > corden_split_int[0] && k[1] > corden_split_int[1] {"NE"}
+         else if k[0] < corden_split_int[0] && k[1] > corden_split_int[1] {"NO"}
+         else if k[0] > corden_split_int[0] && k[1] < corden_split_int[1] {"SE"}
+         else if k[0] < corden_split_int[0] && k[1] < corden_split_int[1] {"SO"} else {"divisa"})
+    }
 
-    println!("{:?}",vetor)
-
+    }
+    for resu in &resultdo {
+        println!("{}",resu );
+    }
 }
