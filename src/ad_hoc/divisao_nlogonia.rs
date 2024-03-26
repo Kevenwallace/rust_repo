e4557001e9aaa126e2e5c6db227a03d62022557a
keven <keven.wallace@paraiba.email>
2024-03-26 10:32:08 -0300

    finish first ad-hoc

--- src/ad_hoc/divisao_nlogonia.rs
+++ src/ad_hoc/divisao_nlogonia.rs
@@ -2,44 +2,38 @@ use std::io;
 
 pub fn divisao_nlogonia() {
     //Exercicio 1091
-    //https://judge.beecrowd.com/pt/problems/view/1091
     let mut resultdo: Vec<&str> = Vec::new();
-
+    let mut entrada_int: i16;
     loop {
-    let mut kuantity:String = String::new();
-    io::stdin().read_line(&mut kuantity).expect("error");
-    let kuantity_int:i16 = kuantity.trim().parse().expect("error parse");
-    if kuantity_int == 0{break;}
-    let mut value_cordenade:String = String::new();
-    io::stdin().read_line(&mut value_cordenade).expect("error");
+    let mut entrada:String = String::new();
     
-    let corden_split:Vec<&str> = value_cordenade.split_whitespace().collect();
-    let mut corden_split_int:Vec<i16> = Vec::with_capacity(2);
-    for l in corden_split {corden_split_int.push(l.trim().parse().expect("error parse"))}
-
-
-    let mut vetor:Vec<Vec<i16>> = Vec::new();
-    for _ in 0..kuantity_int{
+    match io::stdin().read_line(&mut entrada) {
+        Ok(0) => break,
+        Ok(_) => {entrada_int = match entrada.trim().parse() {Ok(num) => num,Err(_) => {println!("weeoe"); return;}}}
+        Err(_) => {println!("error"); return;}
+    };
+    let mut value_coordinates:String = String::new();
+    let mut coordinates_x_y:Vec<i16> = Vec::with_capacity(2);
+    match io::stdin().read_line(&mut value_coordinates) {
+        Ok(_) => {  let coordinate_split:Vec<&str> = value_coordinates.split_whitespace().collect();
+                    for coordinate in coordinate_split 
+                    {coordinates_x_y.push(coordinate.trim().parse().expect("error parse"))}}
+        Err(_) => {println!("error"); return;}
+    };
+    let mut vetor_tester:Vec<Vec<i16>> = Vec::new();
+    for _ in 0..entrada_int{
         let mut item_vec:String = String::new();
         io::stdin().read_line(&mut  item_vec).expect("error no for");
         let split_item_vec:Vec<&str> = item_vec.split_whitespace().collect();
-        
         let mut int_splited: Vec<i16> = Vec::with_capacity(2);
-        for j in split_item_vec{
-            
-            int_splited.push(j.trim().parse().expect("error parse"))
-        }
-        vetor.push(int_splited);
+        for j in split_item_vec{int_splited.push(j.trim().parse().expect("error parse"))}
+        vetor_tester.push(int_splited);
     }
-    for k in &vetor{
-        resultdo.push(if k[0] > corden_split_int[0] && k[1] > corden_split_int[1] {"NE"}
-         else if k[0] < corden_split_int[0] && k[1] > corden_split_int[1] {"NO"}
-         else if k[0] > corden_split_int[0] && k[1] < corden_split_int[1] {"SE"}
-         else if k[0] < corden_split_int[0] && k[1] < corden_split_int[1] {"SO"} else {"divisa"})
+    for coordinates_testing in &vetor_tester{
+        resultdo.push(if coordinates_testing[0] > coordinates_x_y[0] && coordinates_testing[1] > coordinates_x_y[1] {"NE"}
+        else if coordinates_testing[0] < coordinates_x_y[0] && coordinates_testing[1] > coordinates_x_y[1] {"NO"}
+        else if coordinates_testing[0] > coordinates_x_y[0] && coordinates_testing[1] < coordinates_x_y[1] {"SE"}
+        else if coordinates_testing[0] < coordinates_x_y[0] && coordinates_testing[1] < coordinates_x_y[1] {"SO"} else {"divisa"})}
     }
-
-    }
-    for resu in &resultdo {
-        println!("{}",resu );
-    }
+    for resu in &resultdo {println!("{}",resu );}
 }
