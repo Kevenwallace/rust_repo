b103b6bba55d25347885dddc7f4679f526d002d6
keven <keven.wallace@paraiba.email>
2024-04-05 13:03:23 -0300

    finaliza media e mediana

--- src/ad_hoc/media_e_mediana.rs
+++ src/ad_hoc/media_e_mediana.rs
@@ -1,22 +1,43 @@
-use std::io;
+use std::io::{self, BufRead};
 
 pub fn media_e_mediana (){
     //Exercicio 1379
-    let mut entrada:String = String::new();
-    let mut mediana:Vec<i16> = Vec::with_capacity(3);
-    io::stdin().read_line(& mut entrada).expect("error");
-    let entrada_split:Vec<&str> = entrada.split_whitespace().collect();
-    let value_a:i16 = match entrada_split[0].trim().parse() {Ok(num) => num,Err(_) => {println!("error"); return;}};
-    let value_b:i16 = match entrada_split[1].trim().parse() {Ok(num) => num,Err(_) => {println!("error"); return;}};
+    let stdin = io::stdin();
+    let handle = stdin.lock();
+    let mut valores_totais: Vec<Vec<i64>> = Vec::new();
 
-    mediana.push(value_a);
-    mediana.push(value_b);
+    for lines in handle.lines(){
+        if let Ok(entrada) = lines {
+            let valores :Vec<i64> = entrada
+            .trim()
+            .split_whitespace()
+            .map(|val|val.parse::<i64>())
+            .filter_map(Result::ok)
+            .collect();
 
-    let tot = ((value_a + value_b) - (value_a * 3))*-1;
-    mediana.push(tot);
-    mediana.sort();
-    println!("mediana 1 = {}", mediana[1]);
-    if (value_a + value_b + tot) / 3 == mediana[1] {println!("o valot = {} é valido", tot)}
+            if valores.len() < 2 {
+                println!("Erro: Pelo menos dois valores inteiros são necessários");
+                return; // Retorna imediatamente se não houver valores suficientes
+            }
 
-    
-}   
+            let value_a:Option<i64> = Some(valores[0]);
+            let value_b:Option<i64> = Some(valores[1]);
+
+            if let (Some(a), Some(b)) = (value_a, value_b) {
+                if a == 0 && b == 0 {break;}
+                let new_vec: Vec<i64> = vec![a, b];
+                valores_totais.push(new_vec);
+                }
+        }
+    }
+    let mut array_result:Vec<i64> = Vec::new();
+    for valor_unico in valores_totais{
+        let mut x_value:Vec<i64> = Vec::with_capacity(2);
+        for i in &valor_unico {
+                let tot = ((valor_unico[0] + &valor_unico[1]) - (i * 3))*-1; x_value.push(tot);
+        }
+        x_value.sort();
+        array_result.push(x_value[0])
+    }
+    for result in array_result {println!("{}", result)}
+} 
